97eab5bba906b9417f25c2de7e378bc358387b98
Likhit Garimella <likhitgmella@gmail.com>
2021-04-17 13:41:30 +0530

    week-1 done

--- Week-1/ViewController.m
+++ Week-1/ViewController.m
@@ -2,12 +2,17 @@
 //  ViewController.m
 //  Week-1
 //
-//  Created by Likhit Garimella on 16/04/21.
+//  Created by Priya Singh on 16/04/21.
 //
 
+#import "MapKit/MapKit.h"
+#import "WebKit/WebKit.h"
 #import "ViewController.h"
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet MKMapView *myMapView;
+@property (weak, nonatomic) IBOutlet WKWebView *myWebView;
 
 @end
 
@@ -15,8 +20,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    NSString *webURL = @"https://github.com/priya-singh-28";
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:webURL]];
+    [self.myWebView loadRequest:request];
+    
+    double latitude = 28.704060;
+    double longitude = 77.102493;
+    
+    MKPointAnnotation *wAnno = [[MKPointAnnotation alloc] init];
+    wAnno.coordinate = CLLocationCoordinate2DMake(latitude, longitude);
+    wAnno.title = @"Priya Singh";
+    
+    [self.myMapView addAnnotation:wAnno];
+    
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(wAnno.coordinate, 1000, 1000);
+    MKCoordinateRegion adjusted = [self.myMapView regionThatFits:region];
+    [self.myMapView setRegion: adjusted animated: YES];
+    
 }
 
-
-@end
+@end    // #44
